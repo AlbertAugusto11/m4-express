4f0dabee6a37c2d88650638ef93e17377a6128a4
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2025-02-18 21:21:44 -0300

    Update createProductBody.schema.ts

--- src/schemas/createProductBody.schema.ts
+++ src/schemas/createProductBody.schema.ts
@@ -4,3 +4,8 @@ export const createProductBodySchema = z.object({
     name: z.string().min(2),
     price: z.number().min(0.01)
 })
+
+const schemaMod = createProductBodySchema.partial()
+const schemaMod2 = createProductBodySchema.omit({price:true})
+const schemaMod3 = createProductBodySchema.pick({price:true})
+// Estas são formas de usar o Pick, Omit, Partial na bibliotece ZOD
